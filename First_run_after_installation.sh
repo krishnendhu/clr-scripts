f227c1693f09b2089471f4c8180c6fd42c7c0b4c
Krishnendhu Prakasan <krishnendhu2000@gmail.com>
2018-09-25 11:53:44 +0200

    Added vim package and corrected a mistake

--- First_run_after_installation.sh
+++ First_run_after_installation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-useradd tester; echo -e "Aw3s0m3!\nAw3s0m3!" | passwd tester'
-swupd bundle-add os-clr-on-clr desktop-autostart openssh-server os-utils-gui
+useradd tester; echo -e "Aw3s0m3!\nAw3s0m3!" | passwd tester
+swupd bundle-add os-clr-on-clr desktop-autostart openssh-server os-utils-gui vim
 usermod -G wheel -a tester
